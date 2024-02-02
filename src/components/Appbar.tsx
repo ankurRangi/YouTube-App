56514a6f5b2c32bb6f53deed81356fd53b130c88
Ankur <ankur.rangi08@gmail.com>
2024-02-03 01:48:26 +0530

    AppBar Component v1

---
+++ src/components/Appbar.tsx
@@ -0,0 +1,13 @@
+import { SearchBar } from "./Searchbar"
+
+export const Appbar = () => {
+  return (
+    <div className="flex justify-between">
+      <div>YoutTube Logo</div>
+
+      <div><SearchBar /></div>
+
+      <div>Sign in</div>
+    </div>
+  )
+}
